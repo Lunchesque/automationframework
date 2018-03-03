56b6a05f5e5eaf74bd6f5eee0a4f9b86d7118f14
lunchesque <lunchesque1993@gmail.com>
2018-03-03 17:21:45 +0300

    api for login using HandyWrappers class from utilities/handy_wrapper

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -1,21 +1,47 @@
 from selenium.webdriver.common.by import By
+from utilities.handy_wrappers import HandyWrappers
+
+
+#pytest -s -v tests/home/login_test.py
 
 
 class LoginPage():
 
     def __init__(self, driver):
         self.driver = driver
+        self.wrapper = HandyWrappers(self.driver)
+    #locators
+    _login_link = "//a[@href='/sign_in']"
+    _email_field = "//input[@id='user_email']"
+    _password_field = "//input[@id='user_password']"
+    _login_btn = "//input[@type='submit']"
 
-    def login(self, username, password):
+    def getLoginLink(self):
+        return self.wrapper.getElement(self._login_link)
 
-        loginLink = self.driver.find_element(By.XPATH, "//a[@href='/sign_in']")
-        loginLink.click()
+    def getEmailField(self):
+        return self.wrapper.getElement(self._email_field)
 
-        userEmail = self.driver.find_element(By.XPATH, "//input[@id='user_email']")
-        userEmail.send_keys(username)
+    def getPasswordField(self):
+        return self.wrapper.getElement(self._password_field)
 
-        userPassword = self.driver.find_element(By.XPATH, "//input[@id='user_password']")
-        userPassword.send_keys(password)
+    def getLoginBtn(self):
+        return self.wrapper.getElement(self._login_btn)
 
-        loginBtn = self.driver.find_element(By.XPATH, "//input[@type='submit']")
-        loginBtn.click()
+    def clickLoginLink(self):
+        self.getLoginLink().click()
+
+    def enterEmail(self, email):
+        self.getEmailField().send_keys(email)
+
+    def enterPassword(self, password):
+        self.getPasswordField().send_keys(password)
+
+    def clickLoginBtn(self):
+        self.getLoginBtn().click()
+
+    def login(self, email, password):
+        self.clickLoginLink()
+        self.enterEmail(email)
+        self.enterPassword(password)
+        self.clickLoginBtn()
