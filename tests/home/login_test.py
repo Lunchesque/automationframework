5baae0ea7963f782e3a310496bc57f87a74b4793
lunchesque <lunchesque1993@gmail.com>
2018-03-05 17:07:42 +0300

    1

--- tests/home/login_test.py
+++ tests/home/login_test.py
@@ -8,18 +8,18 @@ from pages.home.login_page import LoginPage
 @pytest.mark.usefixtures("oneTimeSetUp", "setUp")
 class LoginTests(unittest.TestCase):
 
-    # @pytest.fixture(autouse=True)
-    # def classSetup(self, oneTimeSetUp):
-    #     self.lp = LoginPage(self.driver)
-    #     self.ts = StatusTest(self.driver)
+    @pytest.fixture(autouse=True)
+    def classSetup(self, oneTimeSetUp):
+        self.lp = LoginPage(self.driver)
+        self.ts = StatusTest(self.driver)
 
-    @pytest.mark.run(order=1)
+    @pytest.mark.run(order=2)
     def test_valid_login(self):
-        result1 = self.verifyLoginTitle()
+        result1 = self.lp.verifyLoginTitle()
         self.ts.mark(result1, "Title verifyed")
-        result2 = self.verifySeccessfulLogin()
+        result2 = self.lp.verifySeccessfulLogin()
         self.ts.markFinal("test_valid_login", result2, "Login was seccessful")
-    #
+
     # @pytest.mark.run(order=1)
     # def test_invalid_login(self):
     #     self.lp.login()
